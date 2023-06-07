881da2dd451115350f5f8f6cba5590be8f518db3
EstefaUf <110433991+EstefaUf@users.noreply.github.com>
2023-06-07 12:21:41 -0300

    Add files via upload

---
+++ src/Components/data.js
@@ -0,0 +1,38 @@
+export const data = [
+	{
+		id: 1,
+		img: 'zorro.jpg',
+		nameProduct: 'Zorrito',
+		price: 2800,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'alf.jpg',
+		nameProduct: 'Alf',
+		price: 6000,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'chavo.jpg',
+		nameProduct: 'El Chavo',
+		price: 6500,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'yoda.jpg',
+		nameProduct: 'Baby Yoda',
+		price: 3500,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'nena.jpg',
+		nameProduct: 'Muñequita Candela',
+		price: 3000,
+		quantity: 1,
+	},
+	
+];
